ec028312ca9d2c5b264e5e6d2a12d71f71dc8bc8
karen <karen.quispe.arana@gmail.com>
2025-01-19 19:01:52 -0500

    componenteslista de tareas, crea rutas

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="sidebar">
+        <h2>Barra lateral</h2>
+        <ul>
+            <li><router-link to="/contador">Contador</router-link></li>
+            <li><router-link to="/lista-de-tareas">Lista de tareas</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+    .sidebar {
+        width: 200px;
+        background-color: #d66cb3;
+        padding: 20px;
+        border-radius: 15px;
+    }
+</style>
