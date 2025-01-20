7dde8c314af33b671ef3fa5f0e8fcc7aaf7a3ba5
karen <karen.quispe.arana@gmail.com>
2025-01-19 19:37:02 -0500

    side bar, un nuevo modulo registro

---
+++ src/modules/registro/views/RegistrarView.vue
@@ -0,0 +1,35 @@
+<template>
+ <div>
+  <h2>Formulario de registro</h2>
+  <Form :validation-schema="schema" @submit="">
+   <div class="form">
+    <label for="name">Nombre:</label>
+    <Field type="text" name="name" id="name" placeholder="ingrese su npombre" />
+    <ErrorMessage name="name"></ErrorMessage>
+   </div>
+   <div class="form">
+    <label for="email">E-mail:</label>
+    <Field type="email" name="email" id="email" placeholder="ingrese su email" />
+    <ErrorMessage name="email"></ErrorMessage>
+   </div>
+   <div class="form">
+    <button type="submit">Registrar</button>
+   </div>
+  </Form>
+ </div>
+</template>
+
+<script setup>
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import { schema } from '../schemas/ValidationSchema';
+
+const onSubmit = () => {
+ console.log('se ah enviado el formulario');
+}
+</script>
+
+<style scoped>
+.form {
+ margin-bottom: 10px;
+}
+</style>
