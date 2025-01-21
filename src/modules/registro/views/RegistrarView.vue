b7d77190b1f0d9eea1b189b55e32d66ad3ac4252
karen <karen.quispe.arana@gmail.com>
2025-01-20 20:40:29 -0500

    store de pinia

--- src/modules/registro/views/RegistrarView.vue
+++ src/modules/registro/views/RegistrarView.vue
@@ -1,15 +1,15 @@
 <template>
  <div>
   <h2>Formulario de registro</h2>
-  <Form :validation-schema="schema" @submit="">
+  <Form :validation-schema="schema" @submit="onSubmit">
    <div class="form">
-    <label for="name">Nombre:</label>
-    <Field type="text" name="name" id="name" placeholder="ingrese su npombre" />
-    <ErrorMessage name="name"></ErrorMessage>
+    <label for="nombre">Nombre:</label>
+    <Field v-model="nombre" type="text" name="nombre" id="nombre" placeholder="ingrese su npombre" />
+    <ErrorMessage name="nombre"></ErrorMessage>
    </div>
    <div class="form">
     <label for="email">E-mail:</label>
-    <Field type="email" name="email" id="email" placeholder="ingrese su email" />
+    <Field v-model="email" type="email" name="email" id="email" placeholder="ingrese su email" />
     <ErrorMessage name="email"></ErrorMessage>
    </div>
    <div class="form">
@@ -22,9 +22,17 @@
 <script setup>
 import { Form, Field, ErrorMessage } from 'vee-validate';
 import { schema } from '../schemas/ValidationSchema';
+import { useRegistrarStore } from '../stores/registrarStore';
+import { ref } from 'vue';
+
+const registrarStore = useRegistrarStore();
+
+const nombre = ref('');
+const email = ref('');
 
 const onSubmit = () => {
- console.log('se ah enviado el formulario');
+  registrarStore.guardarRegistro(nombre.value, email.value);
+  console.log('se ha enviado el formulario');
 }
 </script>
 
